3e82c570bc34f40f3af4ba62022a625c4581de9b
Vinicius Andrade <ozirispc@gmail.com>
2022-06-12 18:53:21 -0400

    Primeiro Subscribe

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,26 @@ import { from } from "rxjs";
 let numbers = [1,5,10];
 let source = from(numbers);
 
+class myObserver {
+  next(x: number) {
+    console.log(x)
+  }
+  error(e: Error) {
+    console.log(e)
+  }
+  complete() {
+    console.log('Complete')
+  }
+}
+
+const myObserverJson = {
+  next: (x: number) => console.log(x),
+  error: (e: Error) => console.log(e),
+  complete: () => console.log('Complete'),
+}
+
 function component() {
-  source.subscribe({
-    next: (x) => console.log(x)
-  })
+  source.subscribe(new myObserver)
 }
 
 component()

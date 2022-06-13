6ae322881b654999fab23f149c469c85946bc594
Vinicius Andrade <ozirispc@gmail.com>
2022-06-13 18:03:36 -0400

    Instância do Observable

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,34 @@
-import { from } from "rxjs";
+import { from, Observable } from "rxjs";
 
 let numbers = [1,5,10];
 let source = from(numbers);
 
-class myObserver {
-  next(x: number) {
-    console.log(x)
-  }
-  error(e: Error) {
-    console.log(e)
-  }
-  complete() {
-    console.log('Complete')
-  }
-}
 
-const myObserverJson = {
-  next: (x: number) => console.log(x),
+let sourceInstance = new Observable(subscriber => {
+  for (let n of numbers) {
+    if (n > 5) {
+      subscriber.error('Aconteceu um erro esperado!');
+    }
+
+    subscriber.next(n);
+    subscriber.complete();
+  }
+  subscriber.complete();
+})
+
+
+const myObserver = {
+  next: (x: number) => { 
+    console.log(x) 
+  },
   error: (e: Error) => console.log(e),
   complete: () => console.log('Complete'),
 }
 
 function component() {
-  source.subscribe(new myObserver)
+  source.subscribe(myObserver)
+
+  sourceInstance.subscribe(myObserver)
 }
 
 component()

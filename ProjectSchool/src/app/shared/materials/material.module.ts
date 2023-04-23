b46e4ca1d34279b6d96fd982848a38ee17dbfab4
Vinicius Andrade <ozirispc@gmail.com>
2023-04-23 11:13:15 -0400

    Limpando Material Modules

---
+++ ProjectSchool/src/app/shared/materials/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatListModule
+  ],
+  declarations: [],
+  providers: []
+})
+export class MaterialModule { }

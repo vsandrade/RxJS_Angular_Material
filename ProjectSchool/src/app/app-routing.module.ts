9e2d052302045b31ef19c6b1157b0a826f853b90
Vinicius Andrade <ozirispc@gmail.com>
2023-04-23 12:11:12 -0400

    module user added

--- ProjectSchool/src/app/app-routing.module.ts
+++ ProjectSchool/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages/users',
+    loadChildren: () =>
+      import('./pages/users/users.module').then(m => m.UsersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ ProjectSchool/src/app/pages/users/users.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styles: [
+  ]
+})
+export class UsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

254fec3f5ea40bd1a1f665c58bfd4f7fe0d1bef5
Vinicius Andrade <ozirispc@gmail.com>
2023-05-15 11:57:42 -0400

    FlexLayout adicionado

--- ProjectSchool/src/app/shared/materials/users-mat.module.ts
+++ ProjectSchool/src/app/shared/materials/users-mat.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   exports: [
     MatIconModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule
   ],
   declarations: [],
   providers: []
--- ProjectSchool/src/app/shared/shared.module.ts
+++ ProjectSchool/src/app/shared/shared.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { ToolbarTitleComponent } from './components/toolbar-title/toolbar-title.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   imports: [
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule
   ],
   declarations: [
     ToolbarTitleComponent

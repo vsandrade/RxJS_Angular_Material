ec71f93775519c1144d003b29e860117f3b8800c
Vinicius Andrade <ozirispc@gmail.com>
2023-05-15 12:07:55 -0400

    Models Criados

---
+++ ProjectSchool/src/app/shared/models/menu.ts
@@ -0,0 +1,29 @@
+import { MenuItem } from "./menuItem";
+
+export const menuItems: MenuItem[] = [
+  {
+    link: '/dashboard',
+    icon: 'fa-chart-line',
+    label: 'Dashboard',
+  },
+  {
+    link: '/courses',
+    icon: 'fa-graduation-cap',
+    label: 'Cursos',
+  },
+  {
+    link: '/teachers',
+    icon: 'fa-person-chalkboard',
+    label: 'Professores',
+  },
+  {
+    link: '/students',
+    icon: 'fa-chalkboard-user',
+    label: 'Alunos',
+  },
+  {
+    link: '/support',
+    icon: 'fa-headset',
+    label: 'Suporte',
+  }
+]
---
+++ ProjectSchool/src/app/shared/models/menuItem.ts
@@ -0,0 +1,5 @@
+export interface MenuItem {
+  link: string;
+  icon: string;
+  label: string;
+}

638a0d86d141bbc64e58b5c43810ffd516513ffd
Vinicius Andrade <ozirispc@gmail.com>
2023-05-22 20:12:10 -0400

    Toolbar-menu - Parte 03 & Desafio

--- ProjectSchool/src/app/shared/materials/material.module.ts
+++ ProjectSchool/src/app/shared/materials/material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   exports: [
@@ -13,7 +14,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatButtonModule,
     MatSidenavModule,
     MatListModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatMenuModule
   ],
   declarations: [],
   providers: []
